48cb2701ff803905eccd7d41049772d7b0144d5d
Iveta <stripeii@uni.coventry.ac.uk>
2017-03-05 19:43:48 +0000

    Simple Pathfinding
    A small grid displays the path taken from A to B.
    Does not prioritise positions closer to the goal yet.

---
+++ Assets/scripts/Graph.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//A graph is used to link the nodes together
+public class Graph
+{
+    public int rows = 0;
+    public int columns = 0;
+    public Node[] nodes;
+
+    //constructor: accepts 2d array
+    public Graph(int[,] grid)
+    {
+        rows = grid.GetLength(0);
+        columns = grid.GetLength(1);
+
+        //Create an empty node for each space in the graph
+        nodes = new Node[grid.Length];
+        for (var i = 0; i < nodes.Length; i++)
+        {
+            var node = new Node();
+            node.label = i.ToString();
+            nodes[i] = node;
+        }
+
+        for (var r = 0; r < rows; r++)
+        {
+            for (var c = 0; c < columns; c++)
+            {
+                var node = nodes[columns * r + c];
+
+                //Open node is represented by 0
+                //Closed node is represented by 1
+                if(grid[r,c] == 1)
+                {
+                    continue;
+                }
+
+                //Up
+                if(r > 0)
+                {
+                    node.adjacent.Add(nodes[columns * (r - 1) + c]);
+                }
+
+                //Right
+                if (c < columns - 1)
+                {
+                    node.adjacent.Add(nodes[columns * r + c + 1]);
+                }
+
+                //Down
+                if (r < rows - 1)
+                {
+                    node.adjacent.Add(nodes[columns * (r + 1) + c]);
+                }
+
+                //Left
+                if (c > 0)
+                {
+                    node.adjacent.Add(nodes[columns * r + c - 1]);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/scripts/Node.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+//Represents the position of each space in the game
+public class Node
+{
+    //Keep track of previous and adjacent nodes
+    //Label nodes so it is easier to track them
+    //Allow to reset a node
+    public List<Node> adjacent = new List<Node>();
+    public Node previous = null;
+    public string label = "";
+    //public int g, h;
+
+    /*public int f
+    {
+        get
+        {
+            return g + h;
+        }
+    }*/
+    public void Clear()
+    {
+        previous = null;
+    }
+}
